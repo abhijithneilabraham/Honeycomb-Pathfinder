69f17aced8b0d19cf520910f6695e90382cef597
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 00:57:20 +0530

    Update cvtracker.py

--- cvtracker.py
+++ cvtracker.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2 
 img=cv2.imread('new.png')
-imgray = cv2.cvtColor(im, cv.COLOR_BGR2GRAY)
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, -1, (0,255,0), 3)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
