5c6b0481cb6c3fbf1d4df0a1f490bf7fb8e27afe
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 00:46:35 +0530

    crooked ideas

--- cvtracker.py
+++ cvtracker.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 
-im=cv2.imread('honey.jpg')
+im=cv2.imread('new.png')
 imgray = cv2.cvtColor(im, cv.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 contours, hierarchy = cv2.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
