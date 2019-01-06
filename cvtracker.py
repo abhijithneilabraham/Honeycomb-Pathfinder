fd678e29fa3f0d8623e114310d11f0a8bc9af64b
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 02:34:49 +0530

    astar upload

--- cvtracker.py
+++ cvtracker.py
@@ -10,7 +10,7 @@ dst = cv2.dilate(dst,None)
 # Threshold for an optimal value, it may vary depending on the image.
 img[dst>0.01*dst.max()]=[0,0,255]
 
-edges = cv2.Canny(img,100,200)
+edges = cv2.Canny(img,100,200)#cannyedge function for detecting the edges . 100 and 200 are threshold values
 
 cv2.imshow('image',img)
 cv2.waitKey(0)#wait for any keypress
