da3d6e9a57438884e4b839f6f171a6fc14c0e5a5
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 00:38:38 +0530

    update cvtracker

---
+++ cvtracker.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2 
+im=cv2.imread('honey.jpg')
+imgray = cv2.cvtColor(im, cv.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
