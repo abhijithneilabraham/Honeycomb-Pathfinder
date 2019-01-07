5758482b847f582aa8bb2e9967298a24faf75935
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 21:45:34 +0530

    struggling

--- cvtracker.py
+++ cvtracker.py
@@ -8,7 +8,7 @@ def draw_circle(event,x,y,flags,param):
         cv2.circle(img,(x,y),1,(255,0,0),-1)
         print(x,y)
         
-img=cv2.imread('new.png')#read the image
+img=cv2.imread('honeycomb.jpg')#read the image
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,0)
 im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)#to greyscale conversion
