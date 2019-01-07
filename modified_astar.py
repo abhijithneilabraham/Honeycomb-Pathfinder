e67ce426381e378a84db59b960103823bef0a418
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 22:21:43 +0530

    angerry

--- modified_astar.py
+++ modified_astar.py
@@ -9,16 +9,54 @@ Created on Mon Jan  7 16:33:00 2019
 I am gonna modify A* as per my own rules
 '''
 
-def distance(x,y):
-    return ((x**2)+(y**2))**.5
+def distance(x,y,a,b):
+    return (((a-x)**2)+((b-y)**2))**.5
 print(distance(3,4))
-def __init__(self, parent=None, position=None):
-    self.parent = parent
-    self.position = position
 
-    self.g = 0
-    self.h = 0
-    self.f = 0
+'''
+now I need a function to move to the shortest path
+'''
+class Node():
+    """A node class for A* Pathfinding"""
 
-def __eq__(self, other):
-    return self.position == other.position
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+    
+start=[20,30] 
+
+end=[100,200]
+def far(start,end):
+    return distance(end[0],start[0],end[1],start[1])
+    
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0    
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+    
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
