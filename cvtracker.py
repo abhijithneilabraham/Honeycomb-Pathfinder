b94ee6ba06db2acaded14071a5e63a867de36b67
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 13:45:19 +0530

    good

--- cvtracker.py
+++ cvtracker.py
@@ -1,5 +1,11 @@
 import numpy as np
 import cv2 
+
+
+def draw_circle(event,x,y,flags,param):
+    if event == cv2.EVENT_MOUSEMOVE:#sadly,in my mac, mouseclick doesnt work.so,please feel free to change this.
+        cv2.circle(img,(x,y),1,(255,0,0),-1)
+        
 img=cv2.imread('new.png')#read the image
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#to greyscale conversion
 gray = np.float32(imgray)#converting to float32 for giving input to cornerharris
@@ -9,17 +15,12 @@ dst = cv2.dilate(dst,None)
 
 # Threshold for an optimal value, it may vary depending on the image.
 img[dst>0.01*dst.max()]=[0,0,255]
-
 edges = cv2.Canny(img,100,200)#cannyedge function for detecting the edges . 100 and 200 are threshold values
-def draw_circle(event,x,y,flags,param):
-         if event == cv2.EVENT_LBUTTONDBLCLK:
-            cv2.circle(img,(x,y),100,(255,0,0),-1)
-     
- 
-
-cv2.namedWindow('circle')
-cv2.setMouseCallback('circle',draw_circle)
-
-cv2.imshow('image',img)
-cv2.waitKey(0)#wait for any keypress
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle)     
+while(1):
+    cv2.imshow('image',img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
 cv2.destroyAllWindows()
+
--- edge_detection.py
+++ edge_detection.py
@@ -6,9 +6,24 @@ Created on Mon Jan  7 01:31:56 2019
 @author: abhijithneilabraham
 """
 import numpy as np
-import cv2 as cv
-img = cv.imread('new.png',0)
-edges = cv.Canny(img,100,200)
-events = [i for i in dir(cv) if 'EVENT' in i]
+import cv2 
+img = cv2.imread('new.png',0)
+edges = cv2.Canny(img,100,200)
+events = [i for i in dir(cv2) if 'EVENT' in i]
 print(events)
 
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    if event == cv2.EVENT_MOUSEMOVE:
+        cv2.circle(img,(x,y),1,(255,0,0),-1)
+
+# Create a black image, a window and bind the function to window
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle)
+
+while(1):
+    cv2.imshow('image',img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
