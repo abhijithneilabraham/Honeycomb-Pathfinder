b19965ddc48a9b6615fd76f2f4bedd43f1ac6cdc
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-07 12:35:16 +0530

    Update cvtracker.py

--- cvtracker.py
+++ cvtracker.py
@@ -11,7 +11,14 @@ dst = cv2.dilate(dst,None)
 img[dst>0.01*dst.max()]=[0,0,255]
 
 edges = cv2.Canny(img,100,200)#cannyedge function for detecting the edges . 100 and 200 are threshold values
-
+def draw_circle(event,x,y,flags,param):
+    global mouseX,mouseY
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img,(x,y),100,(255,0,0),-1)
+        mouseX,mouseY = x,y
+img = np.zeros((512,512,3), np.uint8)
+cv2.namedWindow('circle')
+cv2.setMouseCallback('circle',draw_circle)        
 cv2.imshow('image',img)
 cv2.waitKey(0)#wait for any keypress
 cv2.destroyAllWindows()
